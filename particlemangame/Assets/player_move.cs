ce73a198ed351cb3ff413c2822908d78d7bf9f54
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-09-21 16:44:58 -0400

    Unity project Added
    Commit Changes
    
    Added Unity Project

---
+++ particlemangame/Assets/player_move.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    public float accel;
+    public float jump;
+    public float fall;
+    public float friction;
+    Vector3 moveDirection;
+    Vector3 planarVelocity;
+    Vector3 trueVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Basic Movement
+    // Basic Physics
+    // Basic Interaction
+    // Fixed 2.5D Cameraspace (Crash Bandicoot, Little Nightmares)
+    // Work on Level Transition System
+    // Finish "Eating Method"
+    // Finish Advanced Movement
+}
