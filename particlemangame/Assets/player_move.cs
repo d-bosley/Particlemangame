7f286ef6632bde42ed67f16ae10a3d3e7e4724dd
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-09-21 17:33:51 -0400

    Program Up
    Added stuff

--- particlemangame/Assets/player_move.cs
+++ particlemangame/Assets/player_move.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class player_move : MonoBehaviour
 {
     public float accel;
+    public float speed;
     public float jump;
     public float fall;
     public float friction;
+    public Rigidbody rb;
+    public Animator anim;
+    public Collider caps;
     Vector3 moveDirection;
     Vector3 planarVelocity;
     Vector3 trueVelocity;
+
+    // Basic Movement
+    // Basic Physics
+    // Basic Interaction
+    // Fixed 2.5D Cameraspace (Crash Bandicoot, Little Nightmares)
+    // Work on Level Transition System
+    // Finish "Eating Method"
+    // Finish Advanced Movement
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +33,9 @@ public class player_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float vert = Input.GetAxis("Vertical");
+        float hori = Input.GetAxis("Horizontal");
+        moveDirection = new Vector3(hori, 0, vert) * speed;
+        rb.velocity = moveDirection;
     }
-
-    // Basic Movement
-    // Basic Physics
-    // Basic Interaction
-    // Fixed 2.5D Cameraspace (Crash Bandicoot, Little Nightmares)
-    // Work on Level Transition System
-    // Finish "Eating Method"
-    // Finish Advanced Movement
 }
